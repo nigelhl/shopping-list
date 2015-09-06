c9b7d1c659f26ef693a58bb94c55c0da0e5e28da
nigel <nmillward@tripadvisor.com>
2015-09-06 21:03:57 +0100

    adding jquery

--- js/app.js
+++ js/app.js
@@ -2,10 +2,10 @@ $(document).ready(function(){
 
 	$('.button').click(function(){
 		$('.list').append('<li>' + $('.textbox').val() + '<input type="button" class="delete" value="Delete"/></li>')
-		/*$('.list li').append( $('<button />').addClass('deleteButton').text('Delete'))*/
 		$('.delete').css({"margin-left" : "30px"})
 		$('.textbox').val("")
 	})
+
 	  $(document).on('click', '.delete', function() {
         $(this).parent().remove();
     });
