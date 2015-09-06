b3d5b953aae1fd7619ae588ec80c0130c6aa0383
nigel <nmillward@tripadvisor.com>
2015-09-06 21:10:29 +0100

    status

--- js/app.js
+++ js/app.js
@@ -10,3 +10,9 @@ $(document).ready(function(){
         $(this).parent().remove();
     });
 });
+
+/*Known bugs
+- add an entry without any textbox
+- refresh will clear the screen
+- Max character limit is 20
+- where should delete button be - it's all wonky*/
